490f84b73c0448a18ed63474316209a3fdfdb541
Elias Castañeda <e71az@icloud.com>
2021-03-04 07:35:34 -0700

    Added more assets to the Preload scene

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -82,6 +82,9 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image("blueButton1", "/src/assets/blue_button02.png");
     this.load.image("blueButton2", "/src/assets/blue_button03.png");
     this.load.image("phaserLogo", "/src/assets/logo.png");
+    this.load.image("box", "/src/assets/grey_box.png");
+    this.load.image("checkedBox", "/src/assets/blue_boxCheckmark.png");
+    this.load.audio("bgMusic", ["/src/assets/TownTheme.mp3"]);
   }
 
   create() {}
