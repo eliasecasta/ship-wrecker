b220c37bf7d920c7580172fc127128eb76a598b3
Elias Castañeda <e71az@icloud.com>
2021-03-08 05:02:55 -0700

    Created model class to keep track of the global music and sound options

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import Phaser from "phaser";
 import config from "./Config/config";
 import GameScene from "./Scenes/GameScene";
-import BootScene from "./Scenes/BootScene"
-import PreloaderScene from "./Scenes/PreloaderScene"
-import TitleScene from "./Scenes/TitleScene"
-import OptionsScene from "./Scenes/OptionsScene"
-import CreditsScene from "./Scenes/CreditsScene"
+import BootScene from "./Scenes/BootScene";
+import PreloaderScene from "./Scenes/PreloaderScene";
+import TitleScene from "./Scenes/TitleScene";
+import OptionsScene from "./Scenes/OptionsScene";
+import CreditsScene from "./Scenes/CreditsScene";
+import Model from "./Model";
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
-    this.scene.add('Boot', BootScene)
-    this.scene.add('Preloader', PreloaderScene)
-    this.scene.add('Title', TitleScene)
-    this.scene.add('Options', OptionsScene)
-    this.scene.add('Credits', CreditsScene)
+    const model = new Model();
+    this.globals = { model };
+    this.scene.add("Boot", BootScene);
+    this.scene.add("Preloader", PreloaderScene);
+    this.scene.add("Title", TitleScene);
+    this.scene.add("Options", OptionsScene);
+    this.scene.add("Credits", CreditsScene);
     this.scene.add("Game", GameScene);
-    this.scene.start('Boot');
+    this.scene.start("Boot");
   }
 }
 
